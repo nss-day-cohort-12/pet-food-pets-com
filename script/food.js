db7f015599e774dccc773385f605af7bac2a0adb
Palmer Harrell <palmerharrell@me.com>
2016-02-03 13:35:40 -0600

    Set up nested for loops to go through the dogfood object

--- script/food.js
+++ script/food.js
@@ -14,16 +14,30 @@ function dogfoodLoad() {
 	console.log("dogfood.json successfully loaded!");
 	// console.log(this.responseText);
 	data = JSON.parse(this.responseText);
-	console.log("Data: ",data);
-	console.log("1st Brand Obj: ", data.dog_brands[0]);
-	console.log("1st Brand Name: ", data.dog_brands[0].name);
-	console.log("1st Brand 1st Type Name: ", data.dog_brands[0].types[0].type);
-	console.log("1st Brand 1st Type 1st Volume Name: ", data.dog_brands[0].types[0].volumes[0].name);
-	console.log("1st Brand 1st Type 1st Volume Price: ", data.dog_brands[0].types[0].volumes[0].price);
-
+	// console.log("Data: ",data);
+	// console.log("1st Brand Obj: ", data.dog_brands[0]);
+	// console.log("1st Brand Name: ", data.dog_brands[0].name);
+	// console.log("1st Brand 1st Type Name: ", data.dog_brands[0].types[0].type);
+	// console.log("1st Brand 1st Type 1st Volume Name: ", data.dog_brands[0].types[0].volumes[0].name);
+	// console.log("1st Brand 1st Type 1st Volume Price: ", data.dog_brands[0].types[0].volumes[0].price);
+	loopTest();
 }
 
 function dogfoodFail() {
 	console.log("dogfood.json FAILED to load.");
 }
 
+function loopTest() {
+	console.log("~~~ LOOP TEST ~~~");
+	
+	for (var i = 0; i < data.dog_brands.length; i++) {
+		for (var j = 0; j < data.dog_brands[i].types.length; j++) {
+			for (var k = 0; k < data.dog_brands[i].types[j].volumes.length; k++) {
+				console.log(data.dog_brands[i].name); // Chuck Wagon, Purina
+				console.log("  " + data.dog_brands[i].types[j].type); // All natural, standard, puppy
+				console.log("    " + data.dog_brands[i].types[j].volumes[k].name);
+				console.log("    " + data.dog_brands[i].types[j].volumes[k].price);
+			};
+		};
+	};
+}
